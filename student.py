6119de84712a80e5c7e80695e2607b9fa4b0e428
learnlyticaassessments <assessments@learnlytica.com>
2025-03-24 12:30:15 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,8 @@
+class GroceryManager:
+    def add_product(self, product_list: list, name: str, price: float, quantity: int = 1, *, category="General") -> None:
+        # TODO: Implement this method
+        pass
+
+    def total_inventory_value(self, product_list: list, price_fn: callable) -> float:
+        # TODO: Implement this method
+        pass
